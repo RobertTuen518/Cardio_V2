30a66c19ebcf5fdcabeb39976b274bdf0c8c331c
Demo Master <70443539+RobertTuen518@users.noreply.github.com>
2023-07-07 14:27:09 +0800

    Update DR_DRE_prototype_3rd.py

--- DR_DRE_prototype_3rd.py
+++ DR_DRE_prototype_3rd.py
@@ -148,7 +148,7 @@ with tab1:
         st.success("Predicted Sucessfully")
         
         # Default submit button is set to disable
-         st.session_state.submit_disabled = True
+        st.session_state.submit_disabled = True
     
         # Print the output according to the prediction
         if prediction == 1:
